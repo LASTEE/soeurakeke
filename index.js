0d04ec84eacad6514eca597ed3a13f67dfedca9f
LASTEE <36556456+LASTEE@users.noreply.github.com>
2018-02-17 04:25:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,19 @@ bot.on('message', message =>{
     
     if (message.content === "Ca va ?"){
         message.reply("Encore mal d'hier soir et toi ?");
-        console.log("Commande Salut effectué");
+        console.log("Commande Pétasse effectué");
+    }
+    if (message.content === "C'est une michto"){
+        message.reply("Absolument pas de l'argent j'en ai déja");
+        console.log("Commande Je pèse dans le game effectué");
+    }
+    if (message.content === "T'es une michto"){
+        message.reply("Absolument pas de l'argent j'en ai déja");
+        console.log("Commande Je pèse dans le game effctué");
+    }
+    if (message.content === "Moi ca va pas bien"){
+        message.reply("Pourquoi donc chéri ?");
+        console.log("Commande Pourquoi effectué");
     }
 });
     
